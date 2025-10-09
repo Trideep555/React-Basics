f4eb6b383a3c048f992408adbf3d74f7fe8ecaf4
trideep555@gmail.com <trideep555@gmail.com>
2025-10-09 23:22:54 +0530

    Routing

--- demo1/src/App.jsx
+++ demo1/src/App.jsx
@@ -5,6 +5,9 @@ import Details from './components/Details'
 import Form from './components/Form'
 import Users from './components/Users'
 import Buttons from './components/Buttons'
+import Gallery from './components/Gallery'
+import Navbar from './components/Navbar'
+import {BrowserRouter, Route, Routes}  from 'react-router-dom';
 
 
 
@@ -12,7 +15,13 @@ function App() {
     
   return (
     <>
-    <Buttons />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<><Navbar /><Description/></>} />
+      <Route path="/gallery" element={<><Navbar /><Gallery/></>} />
+      <Route path="/form" element={<><Navbar /><Form/></>} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
